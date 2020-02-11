49ddc3cb3eaa5782a6a7074c4e86740db4a5828a
kupadhyay1212 <60917359+kupadhyay1212@users.noreply.github.com>
2020-02-11 17:11:57 +0545

    Update auto.js

--- auto.js
+++ auto.js
@@ -1,6 +1,6 @@
 var http = require('http');
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Hello World\n');
+  res.end('this is git test\n');
 }).listen(8060, '10.13.194.129');
 console.log('Server running at http://10.13.194.129:8060/');
